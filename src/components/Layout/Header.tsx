ac411fc223d7e097b6446a3531cca25927b723da
dewanshDT <dewansh.dt@gmail.com>
2023-09-30 01:56:13 +0530

    tracking

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -9,8 +9,8 @@ const Header = () => {
     <header className="w-full z-10 flex sticky top-0 dark:bg-neutral-900 bg-neutral-100 flex-col items-center">
       <div className="max-w-6xl p-4 w-full flex justify-between items-center">
         <Link href="/">
-          <div className="text-lg text-neutral-700 dark:text-neutral-300 font-semibold">
-            Dewansh Thakur
+          <div className="text-base tracking-widest uppercase text-neutral-700 dark:text-neutral-300 font-semibold">
+            Dewansh
           </div>
         </Link>
         <button className="capitalize text-sm" onClick={() => toggleTheme()}>
