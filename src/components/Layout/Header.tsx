37b8780674d719a8721e41459b88965cde17d04b
dewanshDT <dewansh.dt@gmail.com>
2023-10-03 20:11:58 +0530

    logo

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -7,9 +7,9 @@ const Header = () => {
   const { toggleTheme, theme } = useTheme()
   return (
     <header className="header w-full border-b-neutral-200 border-b dark:border-b-neutral-900 z-10 flex sticky top-0 flex-col items-center">
-      <div className="max-w-6xl p-4 w-full flex justify-between items-center">
+      <div className="max-w-6xl px-4 py-3 w-full flex justify-between items-center">
         <Link href="/">
-          <div className="text-base uppercase text-neutral-700 dark:text-neutral-300 font-semibold">
+          <div className="text-base tracking-widest uppercase px-[.6em] py-[.02em] bg-red-500 text-neutral-100 font-bold">
             Dewansh
           </div>
         </Link>
