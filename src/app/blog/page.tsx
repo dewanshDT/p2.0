c9dc569b6df24377811731419e88308e8e0e84ca
dewanshDT <dewansh.dt@gmail.com>
2023-10-09 11:40:38 +0530

    date error

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,7 +12,10 @@ export const metadata: Metadata = {
 const page = () => {
   const blogs = allBlogPosts
     .filter((blog) => blog.visible === true)
-    .sort((a, b) => new Date(b.published) - new Date(a.published))
+    .sort(
+      (a, b) =>
+        new Date(b.published).getTime() - new Date(a.published).getTime(),
+    )
   return (
     <div className="flex flex-col mt-10 w-full px-4 max-w-6xl">
       <div className="text-xl font-bold text-neutral-400 dark:text-neutral-700 uppercase tracking-widest">
