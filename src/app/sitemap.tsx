20265337e7535f5bbe9bdfdfd90d19f5bebdefa1
dewanshDT <dewansh.dt@gmail.com>
2023-10-11 12:50:55 +0530

    dynamic sitemap added

---
+++ src/app/sitemap.tsx
@@ -0,0 +1,59 @@
+import { SITE_URL } from "@/lib/constants"
+import { allBlogPosts } from "contentlayer/generated"
+import { MetadataRoute } from "next"
+
+interface SiteMap {
+  url: string
+  lastModified?: string | Date | undefined
+  changeFrequency?:
+    | "yearly"
+    | "monthly"
+    | "weekly"
+    | "always"
+    | "hourly"
+    | "daily"
+    | "never"
+    | undefined
+  priority?: number | undefined
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteMap: SiteMap[] = [
+    {
+      url: SITE_URL,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${SITE_URL}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${SITE_URL}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ]
+
+  const blogs = allBlogPosts
+    .filter((blog) => blog.visible === true)
+    .sort(
+      (a, b) =>
+        new Date(b.published).getTime() - new Date(a.published).getTime(),
+    )
+
+  blogs.forEach((blog) =>
+    siteMap.push({
+      url: `${SITE_URL}/blog/${blog.slug}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    }),
+  )
+
+  return siteMap
+}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,2 @@
 export const AUTHOR_TWITTER_HANDLE = ""
+export const SITE_URL = "https://blog.dewansh.space"
