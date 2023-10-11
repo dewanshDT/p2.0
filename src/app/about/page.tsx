fd4af7bcba4c522db7cfba49716aef293e100217
dewanshDT <dewansh.dt@gmail.com>
2023-10-11 15:51:52 +0530

    profile links

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image"
 import React from "react"
 import me from "../../../public/me.jpeg"
+import { FaXTwitter } from "react-icons/fa6"
+import Link from "next/link"
+import { AUTHOR_TWITTER_HANDLE } from "@/lib/constants"
+import { FaInstagram, FaLinkedinIn } from "react-icons/fa"
+import { AiOutlineMail } from "react-icons/ai"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "About: Dewansh",
+  description: "About Dewansh Thakur",
+}
 
 const about = () => {
   return (
@@ -8,9 +19,9 @@ const about = () => {
       <div className="text-xl font-bold text-neutral-400 dark:text-neutral-700 uppercase tracking-widest">
         About
       </div>
-      <div className="flex flex-col mx-4 my-6">
-        <div className="flex flex-col md:flex-row gap-8">
-          <div className="flex p-2 border-dashed border-neutral-300 dark:border-neutral-800 border-2 w-max">
+      <div className="flex flex-col sm:mx-4 my-6">
+        <div className="flex flex-col md:flex-row gap-10">
+          <div className="flex p-2 border-dashed border-neutral-300 dark:border-neutral-800 border-2 w-max sm:w-auto sm:mr-auto md:mr-0">
             <Image
               src={me}
               alt="Dewansh Thakur picture"
@@ -19,10 +30,42 @@ const about = () => {
             />
           </div>
           <div className="flex flex-col gap-1">
-            <div className="h1 text-2xl font-light">Dewansh Sing Thakur</div>
-            <h2 className="text-sm text-red-500 font-medium">
-              Full Stack Developer
-            </h2>
+            <div className="h1 text-2xl sm:text-4xl font-light">
+              Dewansh Singh Thakur
+            </div>
+            <h2 className="text-red-500 font-semibold">Full Stack Developer</h2>
+            <p className="text-sm font-medium mt-4 dark:text-neutral-300 text-neutral-700  max-w-sm">
+              I&apos;m a full software engineer based in India. I&apos;m
+              currently contributing to open source technologies.
+            </p>
+            <div className="mt-6 flex gap-2">
+              <Link href="mailto:dewansh.dt@gmail.com" target="_blank">
+                <div className="p-2 text-neutral-800 dark:text-neutral-200 dark:bg-neutral-800 bg-neutral-300  cursor-pointer">
+                  <AiOutlineMail />
+                </div>
+              </Link>
+              <Link href={AUTHOR_TWITTER_HANDLE} target="_blank">
+                <div className="p-2 text-neutral-800 dark:text-neutral-200 dark:bg-neutral-800 bg-neutral-300  cursor-pointer">
+                  <FaXTwitter />
+                </div>
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/dewanshthakur/"
+                target="_blank"
+              >
+                <div className="p-2 text-neutral-800 dark:text-neutral-200 dark:bg-neutral-800 bg-neutral-300  cursor-pointer">
+                  <FaLinkedinIn />
+                </div>
+              </Link>
+              <Link
+                href="https://www.instagram.com/dewanshthakur_/"
+                target="_blank"
+              >
+                <div className="p-2 text-neutral-800 dark:text-neutral-200 dark:bg-neutral-800 bg-neutral-300  cursor-pointer">
+                  <FaInstagram />
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const AUTHOR_TWITTER_HANDLE = ""
+export const AUTHOR_TWITTER_HANDLE = "https://twitter.com/ThakurDewansh"
 export const SITE_URL = "https://blog.dewansh.space"
