758be25abda26c00eeaa95de68f4bfa413051439
dewanshDT <dewansh.dt@gmail.com>
2023-10-15 23:53:48 +0530

    robots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,7 @@ const sourceCodePro = Source_Code_Pro({
 })
 
 export const metadata: Metadata = {
-  title: "Dewansh Thakur",
-  description: "Full Stack Developer",
+  robots: "index, follow",
 }
 
 export default function RootLayout({
