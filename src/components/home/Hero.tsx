a2cfa0cfabba0def6cd40fc9a46473d3251676ee
dewanshDT <dewansh.dt@gmail.com>
2023-10-16 04:45:10 +0530

    dshuffle

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -12,14 +12,12 @@ const Hero = () => {
         Dewansh Thakur
       </h1>
       <p className="text-lg tracking-widest uppercase dark:text-neutral-300 max-w-4xl">
-        <ShuffleText>
-          I&apos;m a Web Developer & Designer with a passion for creating
-          innovative products. With over 4 years of experience, I&apos;ve built
-          web apps and designed web pages for clients in US and India. My
-          expertise extends beyond UI design and web development, as I excel in
-          DevOps, full-stack development, and system design, ensuring robust and
-          well-rounded solutions for the market.
-        </ShuffleText>
+        I&apos;m a Web Developer & Designer with a passion for creating
+        innovative products. With over 4 years of experience, I&apos;ve built
+        web apps and designed web pages for clients in US and India. My
+        expertise extends beyond UI design and web development, as I excel in
+        DevOps, full-stack development, and system design, ensuring robust and
+        well-rounded solutions for the market.
       </p>
       <div className="flex my-16 gap-6">
         <Link
