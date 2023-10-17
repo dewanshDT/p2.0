99a25ff5899965ce6d14d8333d0ed5e95a670fe8
dewanshDT <dewansh.dt@gmail.com>
2023-10-18 01:11:08 +0530

    added template width

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,30 +3,26 @@ import { AnimatePresence, motion } from "framer-motion"
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      {/* <AnimatePresence mode="wait"> */}
+    <motion.div
+      className="w-full flex flex-col items-center"
+      initial={{ opacity: 1 }}
+      animate={{ opacity: 1 }}
+    >
+      {children}
       <motion.div
-        // key={pathName}
-        initial={{ opacity: 1 }}
-        animate={{ opacity: 1 }}
-      >
-        {children}
-        <motion.div
-          className="bg-red-500 absolute top-0 left-0 w-full h-screen origin-bottom"
-          initial={{ scaleY: 0 }}
-          animate={{ scaleY: 0 }}
-          exit={{ scaleY: 1 }}
-          transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-        />
-        <motion.div
-          className="bg-red-500 absolute top-0 left-0 w-full h-screen origin-top"
-          initial={{ scaleY: 1 }}
-          animate={{ scaleY: 0 }}
-          exit={{ scaleY: 0 }}
-          transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-        />
-      </motion.div>
-      {/* </AnimatePresence> */}
-    </>
+        className="bg-red-500 absolute top-0 left-0 w-full h-screen origin-bottom"
+        initial={{ scaleY: 0 }}
+        animate={{ scaleY: 0 }}
+        exit={{ scaleY: 1 }}
+        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+      />
+      <motion.div
+        className="bg-red-500 absolute top-0 left-0 w-full h-screen origin-top"
+        initial={{ scaleY: 1 }}
+        animate={{ scaleY: 0 }}
+        exit={{ scaleY: 0 }}
+        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+      />
+    </motion.div>
   )
 }
