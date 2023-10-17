50bbf9f0768218e832e2c0c0f2c62ef91e1a2778
dewanshDT <dewansh.dt@gmail.com>
2023-10-18 01:25:40 +0530

    page transitions

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -10,17 +10,9 @@ export default function Template({ children }: { children: React.ReactNode }) {
     >
       {children}
       <motion.div
-        className="bg-red-500 absolute top-0 left-0 w-full h-screen origin-bottom"
-        initial={{ scaleY: 0 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleY: 1 }}
-        transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-      />
-      <motion.div
-        className="bg-red-500 absolute top-0 left-0 w-full h-screen origin-top"
-        initial={{ scaleY: 1 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleY: 0 }}
+        className="dark:bg-black bg-white absolute top-0 left-0 w-full h-screen origin-top pointer-events-none"
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 }}
         transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
       />
     </motion.div>
