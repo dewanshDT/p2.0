7281a97d5a6ce2c0912d16fe16decda7b9f3d303
dewanshDT <dewansh.dt@gmail.com>
2023-11-19 15:21:15 +0530

    gradient

--- src/sections/home/Hero.tsx
+++ src/sections/home/Hero.tsx
@@ -1,7 +1,12 @@
 import Link from "next/link"
 import React from "react"
 import { twMerge } from "tailwind-merge"
-import { Button, CursorHighlight, StaggerText } from "../../components"
+import {
+  Button,
+  CursorHighlight,
+  GradientCanvas,
+  StaggerText,
+} from "../../components"
 
 interface Props extends React.HtmlHTMLAttributes<HTMLDivElement> {}
 
@@ -17,8 +22,12 @@ const Hero: React.FC<Props> = (props) => {
       <h5 className="text-lg tracking-widest uppercase dark:text-neutral-300">
         Hello there, I&apos;m
       </h5>
-      <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold uppercase">
+      <h1 className="relative text-4xl md:text-6xl lg:text-8xl font-bold uppercase ">
         Dewansh Thakur
+        <GradientCanvas
+          id="dewansh-hero-gradient"
+          className="absolute mix-blend-screen dark:mix-blend-multiply top-0 left-0 w-full h-full"
+        />
       </h1>
       <p className="text-lg tracking-widest uppercase dark:text-neutral-300 max-w-5xl">
         <StaggerText speed={3}>
